fe44963e3f7fb69add608e3b86ba2bf5249c5b1c
hmm274 <106000988+hmm274@users.noreply.github.com>
2024-09-21 00:19:22 +0900

    Version 0.4
    Updated API data

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,7 @@ const API_KEYS = [
     process.env.REACT_APP_API_KEY, process.env.REACT_APP_API_KEY_1, process.env.REACT_APP_API_KEY_2, 
     process.env.REACT_APP_API_KEY_3, process.env.REACT_APP_API_KEY_4, process.env.REACT_APP_API_KEY_5, 
     process.env.REACT_APP_API_KEY_6, process.env.REACT_APP_API_KEY_7, process.env.REACT_APP_API_KEY_8, 
-    process.env.REACT_APP_API_KEY_9
+    process.env.REACT_APP_API_KEY_9, process.env.REACT_APP_API_KEY_10
 ];
 
 const Search = () => {
