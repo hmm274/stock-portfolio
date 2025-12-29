3cde162f8c17cc0d098d2ec21b677b3769dbf948
hmm274 <revanth.sainath@gmail.com>
2025-12-29 15:20:38 +0900

    Version 1.1
    Reconnected API Keys and Supabase Backend

--- src/components/SupabaseClient.js
+++ src/components/SupabaseClient.js
@@ -1,7 +1,7 @@
 import {createClient} from '@supabase/supabase-js';
 
-const supabaseURL = "https://ligiovzhiyccodgxxjlw.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpZ2lvdnpoaXljY29kZ3h4amx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY2NDY4OTMsImV4cCI6MjA0MjIyMjg5M30.uOJHEpfCEmRdUt8i2AfuzjprNwv6_Uuc4Zx_2lpAonI";
+const supabaseURL = "https://qjrxddneaupxyvreuwut.supabase.co";
+const supabaseKey = "sb_publishable_nDMBd3KWG4McK4-QgAiq6g_RWpTkLY6";
 const supabase = createClient(supabaseURL, supabaseKey);
 
 export default supabase;
